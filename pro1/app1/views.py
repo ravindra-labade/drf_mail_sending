b0f3742811fcaf95bea68f92e953e4d86aaa8a8c
Ravi <ravilabade111@gmail.com>
2024-05-18 10:41:46 +0530

    modified views

--- pro1/app1/views.py
+++ pro1/app1/views.py
@@ -1,12 +1,13 @@
 from django.core.mail import send_mail
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.conf import settings
 
 class SendEmailView(APIView):
     def post(self, request):
         username = request.data.get('username')
         subject = 'This is drf mail function'
-        from_email = 'ravilabade111@gmail.com'
+        from_email = settings.EMAIL_HOST_USER
         to = ['labaderavindra1994@gmail.com']
         send_mail(username, subject, from_email, to)
 
